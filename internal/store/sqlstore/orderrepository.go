4a673f0a54d40d5b8eeb6cc016ad00f447981f60
Alexander Neupokoev <admin@armavir.ru>
2022-12-12 22:04:55 +0300

    fix

--- internal/store/sqlstore/orderrepository.go
+++ internal/store/sqlstore/orderrepository.go
@@ -99,7 +99,7 @@ func (o *OrderRepository) GetOrdersForUpgradeStatus() []string {
 
 func (o *OrderRepository) UpdateStatus(order string, accrual float64, status string) error {
 	fmt.Println("UPDATE", order, accrual, status)
-	_, err := o.store.db.Exec("UPDATE orders SET status = $1, accrual = $2 WHERE number = $3", status, accrual, order)
+	_, err := o.store.db.Exec("UPDATE orders SET accrual = $2 WHERE number = $3", accrual, order)
 	if err != nil {
 		return err
 	}
