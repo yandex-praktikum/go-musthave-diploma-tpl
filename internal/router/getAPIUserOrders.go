08e01f28bb84740896d934363e9bf92280701a9a
Demosfenys <Demosfenys@yandex.ru>
2023-02-17 13:39:19 +0300

    test_1

--- internal/router/getAPIUserOrders.go
+++ internal/router/getAPIUserOrders.go
@@ -2,18 +2,21 @@ package router
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
 func (s *serverMart) getAPIUserOrders(c echo.Context) error {
+	fmt.Println("===> getAPIUserOrders")
 	get := c.Get("user")
 	allOrder, err := s.DB.ReadAllOrderAccrualUser(get.(string))
 	if err != nil {
 		c.Response().WriteHeader(http.StatusInternalServerError)
 		return nil
 	}
+	fmt.Println(allOrder)
 	if len(allOrder) == 0 {
 		c.Response().WriteHeader(http.StatusNoContent)
 		return nil
