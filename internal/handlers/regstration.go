a34d1405ca797cd66915f63e48f27d177711085c
Volkov Alexey <Azcarot387@gmail.com>
2024-02-16 22:46:00 +0300

    токен при регистрации

--- internal/handlers/regstration.go
+++ internal/handlers/regstration.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/Azcarot/GopherMarketProject/internal/storage"
+	"github.com/golang-jwt/jwt"
 )
 
 type RegisterRequest struct {
@@ -46,6 +48,20 @@ func Registration() http.Handler {
 			res.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+		payload := jwt.MapClaims{
+			"sub": userData.Login,
+			"exp": time.Now().Add(time.Hour * 72).Unix(),
+		}
+
+		// Создаем новый JWT-токен и подписываем его по алгоритму HS256
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+
+		authToken, err := token.SignedString(jwtSecretKey)
+		if err != nil {
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		res.Header().Add("Authorization", authToken)
 		res.WriteHeader(http.StatusOK)
 	}
 	return http.HandlerFunc(register)
