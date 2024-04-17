bcb208d62bd6ad7c4ec633ac486aeb0384532078
Stas Merzyakov <merzliakov@gmail.com>
2024-04-17 22:32:03 +0400

    наработки

--- internal/config/gophermart.go
+++ internal/config/gophermart.go
@@ -8,14 +8,17 @@ import (
 )
 
 type GophermartConfig struct {
-	TokenExp             time.Duration `env:"JWT_EXP"`
-	TokenSecret          string        `env:"JWT_SECRET"`
-	DatabaseUri          string        `env:"DATABASE_URI"`
-	RunAddress           string        `env:"RUN_ADDRESS"`
-	AccrualSystemAddress string        `env:"ACCRUAL_SYSTEM_ADDRESS"`
-	MaxConns             int           `env:"DATABASE_MAX_CONNS"`
-	ProcessingLimit      int           `env:"ORDER_PROCESSING_LIMIT"`
-	ProcessingScoreDelta time.Duration `env:"ORDER_PROCESSING_DELTA"`
+	TokenExp              time.Duration `env:"JWT_EXP"`
+	TokenSecret           string        `env:"JWT_SECRET"`
+	DatabaseUri           string        `env:"DATABASE_URI"`
+	RunAddress            string        `env:"RUN_ADDRESS"`
+	AccrualSystemAddress  string        `env:"ACCRUAL_SYSTEM_ADDRESS"`
+	AcrualSystemPoolCount int           `env:"ACRUAL_SYSTEM_POOL_COUNT"`
+	MaxConns              int           `env:"DATABASE_MAX_CONNS"`
+	MaxConnLifetime       time.Duration `env:"DATABASE_MAX_CONN_LIFE_TIME"`
+	MaxConnIdleTime       time.Duration `env:"DATABASE_MAX_CONN_IDLE_TIME"`
+	ProcessingLimit       int           `env:"ORDER_PROCESSING_LIMIT"`
+	ProcessingScoreDelta  time.Duration `env:"ORDER_PROCESSING_DELTA"`
 }
 
 func LoadGophermartConfig() (*GophermartConfig, error) {
@@ -23,7 +26,10 @@ func LoadGophermartConfig() (*GophermartConfig, error) {
 
 	flag.StringVar(&srvConf.TokenSecret, "jwtSec", "secret", "jwt secret")
 	flag.DurationVar(&srvConf.TokenExp, "jwtExp", 3*time.Hour, "jwt expiration period")
-	flag.IntVar(&srvConf.MaxConns, "dbMaxCon", 5, "database max connections")
+	flag.IntVar(&srvConf.MaxConns, "dbMaxConns", 5, "database max connections")
+	flag.IntVar(&srvConf.AcrualSystemPoolCount, "accrualPoolCount", 5, "accrual pool count")
+	flag.DurationVar(&srvConf.MaxConnLifetime, "dbMaxConnLifeTime", 5*time.Minute, "database max connection life time")
+	flag.DurationVar(&srvConf.MaxConnIdleTime, "dbMaxConnIdleTime", 5*time.Minute, "database max connection idle time")
 
 	flag.IntVar(&srvConf.ProcessingLimit, "pLimit", 10, "order processing limit")
 	flag.DurationVar(&srvConf.ProcessingScoreDelta, "pDelta", 20*time.Second, "order processing delta")
--- internal/gophermart/app/order.go
+++ internal/gophermart/app/order.go
@@ -5,27 +5,42 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
+	"github.com/StasMerzlyakov/gophermart/internal/config"
 	"github.com/StasMerzlyakov/gophermart/internal/gophermart/domain"
 )
 
-func NewOrder(storage OrderStorage) *order {
-	return &order{
-		storage: storage,
+func NewOrder(ctx context.Context, conf *config.GophermartConfig,
+	storage OrderStorage, acrualSystem AcrualSystem) *order {
+	ord := &order{
+		storage:               storage,
+		processingWorkerCount: conf.AcrualSystemPoolCount,
+		acrualSystem:          acrualSystem,
 	}
+
+	return ord
 }
 
 //go:generate mockgen -destination "./mocks/$GOFILE" -package mocks . OrderStorage
 type OrderStorage interface {
-	Upload(data *domain.OrderData) error
-	Orders(userID int) ([]domain.OrderData, error)
-	Update(number domain.OrderNumber, status domain.OrderStatus, accrual *float64) error
-	ForProcessing(statuses []domain.OrderStatus) ([]domain.OrderData, error)
+	Upload(ctx context.Context, data *domain.OrderData) error
+	Orders(ctx context.Context, userID int) ([]domain.OrderData, error)
+	Update(ctx context.Context, number domain.OrderNumber, status domain.OrderStatus, accrual *float64) error
+	UpdateBatch(ctx context.Context, orders []domain.OrderData) error
+	ForProcessing(ctx context.Context, statuses []domain.OrderStatus) ([]domain.OrderData, error)
+}
+
+//go:generate
+type AcrualSystem interface {
+	Update(ctx context.Context, orderNum domain.OrderNumber) (*domain.AccrualData, error)
 }
 
 type order struct {
-	storage OrderStorage
+	storage               OrderStorage
+	acrualSystem          AcrualSystem
+	processingWorkerCount int
 }
 
 // Загрузка пользователем номера заказа для расчёта
@@ -62,7 +77,7 @@ func (ord *order) New(ctx context.Context, number domain.OrderNumber) error {
 		UploadedAt: domain.RFC3339Time(time.Now()),
 	}
 
-	err = ord.storage.Upload(orderData)
+	err = ord.storage.Upload(ctx, orderData)
 	if err != nil {
 		logger.Infow("order.New", "err", err.Error())
 
@@ -94,7 +109,7 @@ func (ord *order) All(ctx context.Context) ([]domain.OrderData, error) {
 		return nil, domain.ErrUserIsNotAuthorized
 	}
 
-	data, err := ord.storage.Orders(userID)
+	data, err := ord.storage.Orders(ctx, userID)
 	if err != nil {
 		logger.Errorw("order.All", "err", err.Error())
 		return nil, fmt.Errorf("%w: %v", domain.ErrServerInternal, err.Error())
@@ -106,4 +121,134 @@ func (ord *order) All(ctx context.Context) ([]domain.OrderData, error) {
 	}
 
 	return data, nil
+}
+
+func (ord *order) refreshOrderStatus(ctx context.Context,
+	logger domain.Logger,
+	ordNumChan <-chan domain.OrderNumber,
+	acrualDataChan chan<- *domain.AccrualData) {
+
+	// Пытаюсь осмыслить https://go.dev/blog/io2013-talk-concurrency
+	var ordNumInternalChan <-chan domain.OrderNumber
+	var sleepChan <-chan time.Time
+
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Infow("app.refreshOrder", "status", "complete")
+			return
+		case <-sleepChan:
+			// Подождали 5 секунд, можно запускать обработку
+			ordNumInternalChan = ordNumChan
+			sleepChan = nil
+		case orderNum := <-ordNumInternalChan:
+			acrData, err := ord.acrualSystem.Update(ctx, orderNum)
+			if err != nil {
+				// Произошла ошибка - запускаем sleepChan канал в надежде на восстановление
+				logger.Infow("order.refreshOrder", "num", orderNum, "err", err.Error())
+				sleepChan = time.After(5 * time.Second)
+				ordNumInternalChan = nil
+			} else {
+				acrualDataChan <- acrData
+			}
+		}
+	}
+}
+
+func (ord *order) orderStatusUpdater(ctx context.Context, logger domain.Logger, acrualDataChan <-chan *domain.AccrualData) {
+	// Будем обновлять данные пачками либо по 10 записей либо через каждые 2 секунды
+	var orders []domain.OrderData
+
+	// Пытаюсь осмыслить https://go.dev/blog/io2013-talk-concurrency
+	var waitAcrualsTimeoutChan <-chan time.Time
+	var sleepAfterErrChan <-chan time.Time
+	var acrualDataInternalChan <-chan *domain.AccrualData = acrualDataChan
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Infow("app.orderStatusUpdater", "status", "complete")
+			return
+		case <-sleepAfterErrChan:
+			waitAcrualsTimeoutChan = time.After(2 * time.Second)
+			sleepAfterErrChan = nil
+		case <-waitAcrualsTimeoutChan:
+			if len(orders) > 0 {
+				err := ord.storage.UpdateBatch(ctx, orders)
+				if err != nil {
+					// Произошла ошибка - запускаем sleepChan канал в надежде на восстановление
+					logger.Infow("order.UpdateBatch", "err", err.Error())
+					sleepAfterErrChan = time.After(5 * time.Second)
+					acrualDataInternalChan = nil
+				}
+				clear(orders)
+			}
+		case acrualData := <-acrualDataInternalChan:
+			switch acrualData.Status {
+			case domain.AccrualStatusInvalid:
+				orders = append(orders, domain.OrderData{
+					Number: acrualData.Number,
+					Status: domain.OrderStratusInvalid,
+				})
+			case domain.AccrualStatusProcessed:
+				orders = append(orders, domain.OrderData{
+					Number:  acrualData.Number,
+					Status:  domain.OrderStratusProcessing,
+					Accrual: acrualData.Accrual,
+				})
+			}
+		}
+	}
+}
+
+func (ord *order) PoolAcrualSystem(ctx context.Context) {
+	logger, err := domain.GetLogger(ctx)
+	if err != nil {
+		fmt.Printf("app.orderPool error - logger not found")
+		return
+	}
+	var wg sync.WaitGroup
+	defer wg.Done()
+
+	ordNumChan := make(chan domain.OrderNumber)
+	acrualDataChan := make(chan *domain.AccrualData)
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ord.orderStatusUpdater(ctx, logger, acrualDataChan)
+	}()
+
+	for i := 0; i < ord.processingWorkerCount; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ord.refreshOrderStatus(ctx, logger, ordNumChan, acrualDataChan)
+		}()
+	}
+
+Loop:
+	for {
+		orders, err := ord.storage.ForProcessing(ctx, []domain.OrderStatus{domain.OrderStratusNew})
+		if err != nil {
+			logger.Errorw("app.orderPool", "err", err.Error())
+			select {
+			case <-ctx.Done():
+				logger.Infow("app.orderPool", "status", "complete")
+				close(ordNumChan)
+				break Loop
+			case <-time.After(5 * time.Second): // Возможно все восстановится за это время
+				continue
+			}
+		}
+
+		for _, ord := range orders {
+			select {
+			case <-ctx.Done():
+				logger.Infow("app.orderPool", "status", "complete")
+				close(ordNumChan)
+				return
+			case ordNumChan <- ord.Number:
+			}
+		}
+	}
 }
