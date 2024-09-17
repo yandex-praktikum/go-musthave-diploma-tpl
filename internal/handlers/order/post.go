1465f9b86b2c30dfdfa171f990d3515464b0ddf8
Pavel <kamencov@gmail.com>
2024-09-17 07:05:11 +0300

    iter4 edit post orders

--- internal/handlers/order/post.go
+++ internal/handlers/order/post.go
@@ -35,7 +35,7 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	// Считываем тело запроса и записываем в body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		h.log.Error("error order", "error:", err)
+		h.log.Error("error post order", "error:", err)
 		apiError, _ := json.Marshal(customerrors.APIError{Message: "incorrect body"})
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write(apiError)
@@ -45,7 +45,7 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	// Проверяем корректность номера заказа с помощью алгоритма Луна
 	orderNumber := strings.TrimSpace(string(body))
 	if !isLunaValid(orderNumber) {
-		h.log.Error("error order", "error:", "invalid order numbers")
+		h.log.Error("error post order", "error:", "invalid order numbers")
 		apiError, _ := json.Marshal(customerrors.APIError{Message: "invalid order numbers"})
 		w.WriteHeader(http.StatusUnprocessableEntity)
 		w.Write(apiError)
@@ -55,7 +55,7 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	//получаем из Context token пользователя
 	login, ok := r.Context().Value(middleware.LoginContentKey).(string)
 	if !ok || login == "" {
-		h.log.Info("Error = not userID")
+		h.log.Info("Error post order = not userID")
 	}
 
 	//время создания запроса
@@ -64,7 +64,7 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 	// Проверяем заказ в базе
 	if err = h.service.GetUserByAccessToken(orderNumber, login, now, h.addressAccrual); err != nil {
 		if errors.Is(err, customerrors.ErrAnotherUsersOrder) {
-			h.log.Error("error order", "error:", err)
+			h.log.Error("error post order", "error:", err)
 			apiError, _ := json.Marshal(customerrors.APIError{Message: "order number has already been uploaded by another user"})
 			w.WriteHeader(http.StatusConflict)
 			w.Write(apiError)
@@ -72,14 +72,14 @@ func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if errors.Is(err, customerrors.ErrOrderIsAlready) {
-			h.log.Error("error order", "error:", err)
+			h.log.Error("error post order", "error:", err)
 			apiError, _ := json.Marshal(customerrors.APIError{Message: "order number has already been uploaded by this user"})
 			w.WriteHeader(http.StatusOK)
 			w.Write(apiError)
 			return
 		}
 
-		h.log.Error("error order", "error:", err)
+		h.log.Error("error post order", "error:", err)
 		apoError, _ := json.Marshal(customerrors.APIError{Message: "cannot loading order"})
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write(apoError)
