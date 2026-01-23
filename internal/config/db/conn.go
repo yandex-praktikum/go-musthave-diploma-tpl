cb7ee313645f51f2b7d50e1f382107e4faaf760e
tort <tartushkin00@yandex.ru>
2026-01-23 21:00:50 +0300

    test

--- internal/config/db/conn.go
+++ internal/config/db/conn.go
@@ -3,20 +3,16 @@ package db
 import (
 	"context"
 	"database/sql"
-	"embed"
 	"fmt"
-	"log"
-	"path/filepath"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"github.com/pressly/goose/v3"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-var embedMigrations embed.FS
+//var embedMigrations embed.FS
 
 func NewConnection(ctx context.Context, ps string) (*sql.DB, error) {
 
@@ -25,34 +21,35 @@ func NewConnection(ctx context.Context, ps string) (*sql.DB, error) {
 		return nil, fmt.Errorf("ошибка при открытии базы данных %w", err)
 	}
 
-	err = runMigrations(ctx, db)
+	err = migrations(ps)
 	if err != nil {
 		return nil, err
 	}
 
 	return db, nil
 }
-func runMigrations(ctx context.Context, db *sql.DB) error {
-	if err := goose.SetDialect("postgres"); err != nil {
-		return err
-	}
-	goose.SetLogger(goose.NopLogger())
-	goose.SetBaseFS(embedMigrations)
 
-	migrationsDir := "migrations"
-
-	log.Printf("applying goose migrations from %s", migrationsDir)
-	if err := goose.UpContext(ctx, db, migrationsDir); err != nil {
-		return fmt.Errorf("goose up: %w", err)
-	}
-	return nil
-}
+//func runMigrations(ctx context.Context, db *sql.DB) error {
+//	if err := goose.SetDialect("postgres"); err != nil {
+//		return err
+//	}
+//	goose.SetLogger(goose.NopLogger())
+//	goose.SetBaseFS(embedMigrations)
+//
+//	migrationsDir := "migrations"
+//
+//	log.Printf("applying goose migrations from %s", migrationsDir)
+//	if err := goose.UpContext(ctx, db, migrationsDir); err != nil {
+//		return fmt.Errorf("goose up: %w", err)
+//	}
+//	return nil
+//}
 
 func migrations(ps string) error {
-
+	fmt.Println("миграте")
 	// Запуск миграций при старте приложения
 	m, err := migrate.New(
-		"file:"+filepath.Join("..", "migrations"),
+		"file:migrations",
 		ps)
 	if err != nil {
 		//log.Fatalf("Ошибка создания объекта миграции: %v", err)
